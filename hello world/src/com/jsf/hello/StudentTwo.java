9822a2481ce0535e1e83ace1348a61c884ab3127
marwinmd <marwinwiechert@icloud.com>
2016-09-19 15:39:47 +0200

    dropdown list

---
+++ hello world/src/com/jsf/hello/StudentTwo.java
@@ -0,0 +1,40 @@
+package com.jsf.hello;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentTwo {
+	private String firstName;
+	private String lastName;
+	private String country;
+
+	// create no-arg constructor
+	public StudentTwo() {
+	}
+
+	// define getter/setter methods
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
